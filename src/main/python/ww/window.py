d845fe0874c733e4bacd2f9d389927cd97184165
jbcaud <jbcaud@gmail.com>
2020-11-11 19:53:03 -0600

    Added 6-day forecast functionality and removed deploy directory

--- src/main/python/ww/window.py
+++ src/main/python/ww/window.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
+import datetime
 import requests
 import math
 
@@ -17,6 +18,7 @@ class window(QWidget):
         self.height = 400
         self.setFixedWidth(self.width)
         self.setFixedHeight(self.height)
+        self.setContentsMargins(0,0,0,0)
 
         #set background image
         oImage = QPixmap.fromImage(self.ctx.back)
@@ -24,51 +26,57 @@ class window(QWidget):
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(sImage))
         self.setPalette(palette)
-        #self.setStyleSheet("background-color: #3E5BFA;")
 
         #initialize layouts and labels
         citylayout = QHBoxLayout()
+        self.futlayout = QHBoxLayout()
         windlayout = QVBoxLayout()
         self.labelname = QLabel()
-        self.labeltemp = QLabel()
-        self.labeltype = QLabel()
+        self.maintemp = QLabel()
+        self.maintype = QLabel()
 
         #set fonts and colors of labels
         self.labelname.setFont(QFont('Lucida Handwriting', self.width/10 * 3/5))
         self.labelname.setStyleSheet("color: white;")
-        self.labeltemp.setFont(QFont('Lucida Handwriting', self.height/10 * 3/5))
-        self.labeltemp.setStyleSheet("color: white;")
+        self.maintemp.setFont(QFont('Lucida Handwriting', self.height/10 * 3/5))
+        self.maintemp.setStyleSheet("color: white;")
 
         #set up the "city layout" (weather and pic)
         citylayout.addStretch(3)
-        citylayout.addWidget(self.labeltype)
+        citylayout.addWidget(self.maintype)
         citylayout.addStretch()
-        citylayout.addWidget(self.labeltemp)
+        citylayout.addWidget(self.maintemp)
         citylayout.addStretch(3)
 
         #set up the window layout (everything)
         windlayout.addWidget(self.labelname)
         windlayout.setAlignment(self.labelname, Qt.AlignHCenter)
         windlayout.addLayout(citylayout)
-        windlayout.addStretch()
+        windlayout.addStretch(2)
+        frame = QFrame()
+
+        frame.setStyleSheet("background-color: black;")
+        frame.setStyleSheet("background-color: rgba(0,0,0,160);")
+        frame.setLayout(self.futlayout)
+        frame.resize(500,500)
+        windlayout.addWidget(frame)
+        windlayout.addStretch(5)
+
+        #get request from api
+        request = requests.get("http://api.openweathermap.org/data/2.5/onecall?lat=33.300121&lon=-87.500282&APPID=c8acf3c2bf38a339b2495a3121255604&units=imperial")
+        dictionary = request.json() #convert to dictionary
 
         #fill labels with information
-        self.getInfo()
+        self.setMain(dictionary)
+        self.setFuture(dictionary)
+
 
         #set layout
+        windlayout.setContentsMargins(0,0,0,0)
         self.setLayout(windlayout)
 
-    #executes when window set up
-    def getInfo(self):
-        #get request from api
-        r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Tuscaloosa,al,us&APPID=c8acf3c2bf38a339b2495a3121255604&units=imperial")
-        dict = r.json() #convert to dictionary
-
-        #store info in variables
-        temp = str(math.floor(round(dict['main']['temp'])))
-        name = dict['name']
-        ty = dict['weather'][0]['main']
-
+    #sets the type of weather (image)
+    def setType(self, ty, labeltype):
         #set picture and size of type label
         ratiow = self.width / 10
         ratioh = self.height / 10
@@ -79,25 +87,63 @@ class window(QWidget):
                 pixmap = self.ctx.moon
             else: #otherwise use sun pic
                 pixmap = self.ctx.sun
-            self.labeltype.setPixmap(pixmap.scaled(ratiow, ratioh, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow , ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
         elif (ty == "Clouds"):
             pixmap = self.ctx.cloudy
-            self.labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 4.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
         elif (ty == "Rain" or ty == "Drizzle"):
             pixmap = self.ctx.rainy
-            self.labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
         elif (ty == "Thunderstorm"):
             pixmap = self.ctx.thunderstorm
-            self.labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
         elif (ty == "Snow"):
             pixmap = self.ctx.snow
-            self.labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
         else:
             pixmap = self.ctx.other
-            self.labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 4.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+            labeltype.setPixmap(pixmap.scaled(ratiow * 6/5, ratioh * 4.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+
+    #set up current forecast
+    def setMain(self, dict):
+        #store info in variables
+        temp = str(math.floor(round(dict['current']['temp'])))
+        name = "Tuscaloosa"
+        type = dict['current']['weather'][0]['main']
+        self.setType(type, self.maintype)
 
         #set name and temperature labels
         self.labelname.setText(name)
-        self.labeltemp.setText(temp+"°")
+        self.maintemp.setText(temp+"°")
 
+    def setDay(self, dict, num):
+        #store info in variables
+        temp = str(math.floor(round(dict['daily'][num]['temp']['day'])))
+        type = dict['daily'][num]['weather'][0]['main']
+        self.setType(type,self.currtype)
+        if (type == "Clear"): self.currtype.setPixmap(self.ctx.sun.scaled(self.width / 10, self.height / 10 * 5.5/5, transformMode=QtCore.Qt.SmoothTransformation))
+        #set name and temperature labels
+        self.currtemp.setText(temp+"°")
 
+    def setFuture(self, dict):
+        for i in range(0, 6):
+            currday = datetime.date.today() + datetime.timedelta(days = i + 1)
+            forday = currday.strftime("%m/%d")
+            daynum = QLabel()
+            daynum.setText(forday)
+            daylayout = QVBoxLayout()
+            self.currtype = QLabel()
+            self.currtemp = QLabel()
+            daynum.setFont(QFont('Lucida Handwriting', self.height/10 * 2/5))
+            daynum.setStyleSheet("color: white; background-color: rgba(255, 255, 255, 0);")
+            self.currtemp.setFont(QFont('Lucida Handwriting', self.height/10 * 3/5))
+            self.currtemp.setStyleSheet("color: white; background-color: rgba(255, 255, 255, 0);")
+            self.currtype.setStyleSheet("background-color: rgba(255,255,255,0);")
+            self.setDay(dict, i)
+            daylayout.addWidget(daynum)
+            daylayout.addWidget(self.currtype)
+            daylayout.addWidget(self.currtemp)
+            daylayout.setAlignment(daynum, Qt.AlignTop)
+            daylayout.setAlignment(self.currtype, Qt.AlignCenter)
+            self.futlayout.addLayout(daylayout)
+            if i < 5: self.futlayout.addStretch(10);
